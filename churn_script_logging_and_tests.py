6ed5b11455222729dd8afafab0e9ed918431a59a
Youhee <youhee.kil@gmail.com>
2022-01-08 23:16:08 +0100

    cm2

--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -1,4 +1,3 @@
-import os
 import logging
 from churn_library import import_data, perform_eda, encoder_helper, perform_feature_engineering, train_models
 import constants
@@ -6,80 +5,83 @@ import constants
 
 logging.basicConfig(
     filename='./logs/churn_library.log',
-    level = logging.INFO,
+    level=logging.INFO,
     filemode='w',
     format='%(name)s - %(levelname)s - %(message)s')
 
-def test_import(import_data):
-	'''
-	test data import - this example is completed for you to assist with the other test functions
-	'''
-	try:
-		df = import_data(constants.DATA_PATH)
-		logging.info("Testing import_data: SUCCESS")
-	except FileNotFoundError as err:
-		logging.error("Testing import_eda: The file wasn't found")
-		raise err
 
-	try:
-		assert df.shape[0] > 0
-		assert df.shape[1] > 0
-	except AssertionError as err:
-		logging.error("Testing import_data: The file doesn't appear to have rows and columns")
-		raise err
+def test_import(import_data):
+    '''
+    test data import - this example is completed for you to assist with the other test functions
+    '''
+    try:
+        df = import_data(constants.DATA_PATH)
+        logging.info("Testing import_data: SUCCESS")
+    except FileNotFoundError as err:
+        logging.error("Testing import_eda: The file wasn't found")
+        raise err
+
+    try:
+        assert df.shape[0] > 0
+        assert df.shape[1] > 0
+    except AssertionError as err:
+        logging.error(
+            "Testing import_data: The file doesn't appear to have rows and columns")
+        raise err
+    return df
 
 
 def test_eda(perform_eda):
-	'''
-	test perform eda function
-	'''
-	try:
-		perform_eda(df = )
-		logging.info("Testing perform_eda: SUCCESS")
+    '''
+    test perform eda function - test creation of images related eda
+    '''
+    perform_eda(df=import_data(constants.DATA_PATH))
+    for col in constants.CATEGORY_LST:
+            try:
+                 with open(f"{constants.EDA_FILEPATH}{col}.png", 'r'):
+                     logging.info("Testing perform_eda: SUCCESS")
+            except FileNotFoundError as err:
+                logging.error("Testing perform_eda: generated images missing")
+                raise err
 
 
 def test_encoder_helper(encoder_helper):
-	'''
-	test encoder helper
-	'''
-	try:
-		encoded_data = encoder_helper(df = import_data(constants.DATA_PATH), category_lst = constants.CATEGORY_LST, response=constants.RESPONSE)
-		logging.info("Testing test_encoder_helper: SUCCESS")
-	except FileNotFoundError as err:
-		logging.error("Testing test_encoder_helper: The file wasn't found")
-		raise err
-	
-	try: 
-		assert isinstance(encoded_data[f"{constants.CATEGORY_LST[0]}_Churn"], float)
-		assert isinstance(encoded_data[f"{constants.CATEGORY_LST[1]}_Churn"], float)
-		logging.info("Testing test_encoder_helper: SUCCESS")
-	except AssertionError as err:
-		logging.error("Testing test_encoder_helper: The values of the encoded variable are not float")
-		raise err
+    '''
+    test encoder helper
+    '''
+    try:
+        encoded_data = encoder_helper(import_data(constants.DATA_PATH), constants.CATGORY_LST, constants.RESULTS)
+        logging.info("Testing test_encoder_helper: SUCCESS")
+    except KeyError as err:
+        logging.error("Testing test_encoder_helper: The file doesn't appear to have rows and columns")
+    
+    try:
+        assert encoded_data.shape[0] > 0
+        assert encoded_data.shape[1] > 0
+    except AssertionError as err:
+        logging.error("Testing test_encoder_helper: The dataframe has an issue with containig either rows or columns")
+        raise err
+    
+    try:
+        assert isinstance(
+            encoded_data[f"{constants.CATEGORY_LST[0]}_Churn"], float)
+        assert isinstance(
+            encoded_data[f"{constants.CATEGORY_LST[1]}_Churn"], float)
+    except AssertionError as err:
+        logging.error(
+            "Testing test_encoder_helper: The values of the encoded variable are not float")
+        raise err
 
 
 def test_perform_feature_engineering(perform_feature_engineering):
-	'''
-	test perform_feature_engineering
-	'''
-	pass
+    '''
+    test perform_feature_engineering
+    '''
+    pass
 
 
 def test_train_models(train_models):
-	'''
-	test train_models
-	'''
-	pass
-
-
-if __name__ == "__main__":
-	test_import(import_data)
-	test_encoder_helper(encoder_helper)
-
-
-
-
-
-
-
-
+    '''
+    test train_models
+    '''
+    pass
--- scratch.py
+++ scratch.py
@@ -1,22 +1,7 @@
 
 if __name__ == "__main__":
-    data = import_data(constants.PATH)
-    perform_eda(df=data)
-    new_data = encoder_helper(
-        data,
-        category_lst=constants.CATEGORY_LST,
-        response=constants.RESPONSE)
-    X_train, X_test, y_train, y_test = perform_feature_engineering(
-        df=new_data, response=constants.RESPONSE)
-    y_train_preds_lr, y_test_preds_lr, y_train_preds_rf, y_test_preds_rf = train_models(
-        X_train=X_train, X_test=X_test, y_train=y_train, y_test=y_test)
-    classification_report_image(y_train=y_train,
-                                y_test=y_test,
-                                y_train_preds_lr=y_train_preds_lr,
-                                y_train_preds_rf=y_train_preds_rf,
-                                y_test_preds_lr=y_test_preds_lr,
-                                y_test_preds_rf=y_test_preds_rf)
-    feature_importance_plot(
-        model=joblib.load('./models/rfc_model.pkl'),
-        X_data=X_test,
-        output_pth=constants.RESULTS_FILEPATH)
+    data_df = import_data(constants.DATA_PATH)
+    perform_eda(data_df)
+    encoded_data_df = encoder_helper(data_df, constants.CATEGORY_LST, constants.RESPONSE)
+    x_train_, x_test_, y_train_, y_test_ = perform_feature_engineering(encoded_data_df, constants.RESPONSE)
+    train_models(x_train_, x_test_, y_train_, y_test_)
